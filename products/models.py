e7f2fc3b9587566f67db3c716b8b689239bc92e6
riassuc <riassuc@gmail.com>
2021-06-08 17:56:40 +0900

    Modify: 클래스 변수명 수정

--- products/models.py
+++ products/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Product(models.Model):
-    title        = models.CharField(max_length=45)
-    sub_title    = models.CharField(max_length=100)
-    price        = models.DecimalField(max_digits=8, decimal_places=2)
-    gram         = models.DecimalField(max_digits=6, decimal_places=1)
-    calorie      = models.DecimalField(max_digits=6, decimal_places=1)
-    stock        = models.IntegerField()
-    sell_count   = models.IntegerField()
-    created_time = models.DateTimeField(auto_now_add=True)
+    title      = models.CharField(max_length=45)
+    sub_title  = models.CharField(max_length=100)
+    price      = models.DecimalField(max_digits=8, decimal_places=2)
+    gram       = models.DecimalField(max_digits=6, decimal_places=1)
+    calorie    = models.DecimalField(max_digits=6, decimal_places=1)
+    stock      = models.IntegerField()
+    sell_count = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'products'
@@ -72,12 +72,12 @@ class Like(models.Model):
         db_table = 'likes'
 
 class Review(models.Model):
-    image_url    = models.URLField()
-    content      = models.TextField()
-    created_time = models.DateTimeField(auto_now_add=True)
-    star_rating  = models.DecimalField(max_digits=2, decimal_places=1)
-    user         = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    product      = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image_url   = models.URLField()
+    content     = models.TextField()
+    created_at  = models.DateTimeField(auto_now_add=True)
+    star_rating = models.DecimalField(max_digits=2, decimal_places=1)
+    user        = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    product     = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     class Meta():
         db_table = 'reviews'
