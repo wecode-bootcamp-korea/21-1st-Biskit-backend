c31eee44b6d751869c7cec30e333ac5bda372dbc
김민규 <skystar_pisces@gimmingyuui-MacBookAir.local>
2021-06-09 16:49:52 +0900

    Add: 리뷰기능구현중

--- products/views.py
+++ products/views.py
@@ -3,19 +3,22 @@ import json
 from django.views import View
 from django.http  import JsonResponse
 
-from .models import Product
+from .models import Product 
 
 class ProductDetailView(View):
 
-    def get(self, request, ):
+    def get(self, request):
 
             product = Product.objects.get(id=1)
-
-            tastes            = product.tasteproduct_set.all()
-            images            = product.productimage_set.all()
-            descriptions      = product.description_set.all()
+            
+            reviews      = product.review_set.all()
+            tastes       = product.tasteproduct_set.all()
+            images       = product.productimage_set.all()
+            descriptions = product.description_set.all()
+            
             detail_image_list = []
             taste_list        = []
+            review_list       = []
 
             for image in images:
                 detail_image_list.append(image.image_url)
@@ -24,22 +27,34 @@ class ProductDetailView(View):
                 taste_list.append(taste.taste.taste)
 
             product_info = {
-                'title' : product.title,
-                'sub_title' : product.sub_title,
-                'price' : product.price,
-                'gram' : product.gram,
-                'calorie' : product.calorie,
+                'title'        : product.title,
+                'sub_title'    : product.sub_title,
+                'price'        : product.price,
+                'gram'         : product.gram,
+                'calorie'      : product.calorie,
                 'detail_image' : detail_image_list,
-                'taste' : taste_list
+                'taste'        : taste_list
             }
 
             for description in descriptions:
                 
                 description_info = {
-                    'detail' : description.detail,
+                    'detail'      : description.detail,
                     'information' : description.information,
-                    'delivery' : description.delivery,
-                    'refund' : description.refund
+                    'delivery'    : description.delivery,
+                    'refund'      : description.refund
                 }
-        
-            return JsonResponse({'detail_result' : product_info, 'description_result' : description_info, 'message' : 'SUCCESS'}, status=200)
+
+            for review in reviews:
+                
+                review_info = {
+                    'user'         : review.user.name,
+                    'review_image' : review.image_url,
+                    'content'      : review.content,
+                    'created_at'   : review.created_at,
+                    'star_rating'  : review.star_rating
+                }
+
+                review_list.append(review_info)
+
+            return JsonResponse({'detail_result' : product_info, 'description_result' : description_info, 'review_detail' : review_list, 'message' : 'SUCCESS'}, status=200)
