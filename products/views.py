29a82d1e2b53eb5eb73dbe9f7ef6758adca628f6
riassuc <riassuc@gmail.com>
2021-06-10 17:10:22 +0900

    Modify: 리팩토링

--- products/views.py
+++ products/views.py
@@ -1,33 +1,20 @@
 from django.views     import View
 from django.http      import JsonResponse
 
-from products.models import Product
+from products.models  import Product
 
 class ProductList(View):
-
     def get(self, request):
-
         products = Product.objects.all()
-        
-        result = []
 
-        for product in products:
+        result = [{
+            'title'     : product.title,
+            'sub_title' : product.sub_title,
+            'price'     : product.price,
+            'calorie'   : product.calorie,
+            'gram'      : product.gram,
+            'taste'     : [taste.taste.taste for taste in product.tasteproduct_set.all()] ,
+            'images'    : product.productimage_set.all().first().image_url
+        } for product in products]
 
-            images     = product.productimage_set.all()
-            image_list = [image.image_url for image in images]
-            tastes     = product.tasteproduct_set.all()
-            taste      = [taste.taste.taste for taste in tastes]
-
-            product_info = {
-                'title'     : product.title,
-                'sub_title' : product.sub_title,
-                'price'     : product.price,
-                'calorie'   : product.calorie,
-                'gram'      : product.gram,
-                'taste'     : taste,
-                'images'    : image_list
-            }
-
-            result.append(product_info)
-
-        return JsonResponse({'message' : 'SUCCESS', 'result' : result}, status=200)
+        return JsonResponse({'result' : result}, status=200)
