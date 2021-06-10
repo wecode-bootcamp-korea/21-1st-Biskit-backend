e4a63478f2a959ccb445d2cafa0df266babdede7
김민규 <skystar_pisces@gimmingyuui-MacBookAir.local>
2021-06-10 13:10:06 +0900

    Modify: Restful한 경로로 바꿈

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import ProductDetailView
 
 urlpatterns = [
-    path('/<str:product_title>/detail', ProductDetailView.as_view())
+    path('/<str:product_title>', ProductDetailView.as_view())
 ]
