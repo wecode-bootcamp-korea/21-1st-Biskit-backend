9f783815f8d86f4ca792dd4045c63e4cba8aced9
riassuc <riassuc@gmail.com>
2021-06-10 09:05:53 +0900

    Add: 전체상품 조회페이지 구현

--- products/urls.py
+++ products/urls.py
@@ -1,3 +1,6 @@
-#from django.urls import path
+from django.urls import path
 
-urlpatterns = []
+from products.views import ProductList
+urlpatterns = [
+    path('/products', ProductList.as_view())
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import Q
+
+from products.models import Product
+
+class ProductList(View):
+
+    def get(self, request):
+
+        products = Product.objects.all()
+        
+        result = []
+
+        for product in products:
+
+            images     = product.productimage_set.all()
+            image_list = [image.image_url for image in images]
+            tastes     = product.tasteproduct_set.all()
+            taste      = [taste.taste.taste for taste in tastes]
+
+            product_info = {
+                'title'     : product.title,
+                'sub_title' : product.sub_title,
+                'price'     : product.price,
+                'calorie'   : product.calorie,
+                'gram'      : product.gram,
+                'taste'     : taste,
+                'images'    : image_list
+            }
+
+            result.append(product_info)
+
+        return JsonResponse({'message' : 'SUCCESS'}, status=200)
