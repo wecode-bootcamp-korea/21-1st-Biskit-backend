cf53cd117665bef30aca83e2cfbeed46673cfa7e
AAAAA Ran <aaaaaran@AAAAAui-MacBookPro.local>
2021-06-15 11:32:35 +0900

    add: 중복검사 구현

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views      import SignUpView
+from .views      import SignUpView, AccountCheckView
 
 urlpatterns = [
+    path('/validaccount', AccountCheckView.as_view()),
     path('/signup', SignUpView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -5,10 +5,23 @@ import bcrypt
 
 from django.views     import View
 from django.http      import JsonResponse
-from django.db.models import Q
 
-from biskit_settings  import SECRET_KEY
 from .models          import User
+
+class AccountCheckView(View):
+    def post(self,request):
+        data = json.loads(request.body)
+
+        try:
+            account  = data['account']
+
+            if User.objects.filter(account = account).exists():
+                return JsonResponse({'message':'ALREADY_EXISTS'}, status=400)
+            else: 
+                return JsonResponse({'message':'SUCCESS'}, status=200) 
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
 
 class SignUpView(View):
     def post(self,request):
@@ -28,22 +41,12 @@ class SignUpView(View):
             mobile_Regex    = '\d{3,4}-\d{4}'
             name_Regex      = '[가-힣]'
             hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-             
-            q = Q()
 
             if ('' == account) or ('' == password):
                 return JsonResponse({'message':'VALUE_IS_EMPTY'}, status=400)
-            if not re.match(account_Regex, account):
-                return JsonResponse({'message':'INVALID_ACCOUNT_FORMAT'}, status=400)
-            if not re.match(name_Regex, name):
-                return JsonResponse({'message':'INVALID_NAME_FORMAT'}, status=400)
-            if not re.match(password_Regex, password):
-                return JsonResponse({'message':'INVALID_PASSWORD_FORMAT'}, status=400)
-            if not re.match(email_Regex, email):
-                return JsonResponse({'message':'INVALID_EMAIL_FORMAT'}, status=400)   
-            if not re.match(mobile_Regex, mobile):
-                return JsonResponse({'message':'INVALID_MOBILE_FORMAT'}, status=400)
-            if User.objects.filter(Q(account = account) | Q(mobile = mobile) | Q(email = email)).exists():
+            if not re.match (account_Regex, account) or (name_Regex, name) or (email_Regex, email) or (password_Regex, password) or (mobile_Regex, mobile):
+                return JsonResponse({'message':'INVALID_FORMAT'}, status=400)   
+            if User.objects.filter(account = account).exists():
                 return JsonResponse({'message':'ALREADY_EXISTS'}, status=400)
 
             User.objects.create(
