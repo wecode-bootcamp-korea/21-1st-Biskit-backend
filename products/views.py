422eb06dac72c2ae7a7a685d640861f9d0382631
AAAAA Ran <aaaaaran@AAAAAui-MacBookPro.local>
2021-06-15 14:17:03 +0900

    수정사항 반영

--- products/views.py
+++ products/views.py
@@ -1,13 +1,14 @@
-from django.views          import View
-from django.http           import JsonResponse
-from django.db.models      import Avg, Count
+from django.views import View
+from django.http  import JsonResponse
 
-from .models               import Product
+from .models import Product
 
 class ProductDetailView(View):
-    def get(self, request, product_id):
-        product = Product.objects.get(id=product_id)
+    def get(self, request, product_title):
+
+        product = Product.objects.get(title=product_title)
         
+        tastes       = product.tasteproduct_set.all()
         images       = product.productimage_set.all()
         descriptions = product.description_set.all()
         
@@ -18,7 +19,7 @@ class ProductDetailView(View):
             'gram'             : product.gram,
             'calorie'          : product.calorie,
             'detail_image'     : [image.image_url for image in images],
-            'taste'            : product.tasteproduct_set.first().taste.name,
+            'taste'            : [taste.taste.name for taste in tastes],
             'description_info' : [{
                                     'detail'      : description.detail,
                                     'information' : description.information,
@@ -28,29 +29,3 @@ class ProductDetailView(View):
         }
 
         return JsonResponse({'result' : product_info}, status=200)
-
-class ProductReviewVeiw(View):
-    def get(self, request, product_id):
-        order   = request.GET.get('sort', 'id')
-        page    = int(request.GET.get('page', 1))
-        product = Product.objects.get(id=product_id)
-    
-        reviews = product.review_set.order_by(order)
-
-        review_info = [{'user'         : review.user.name,
-                        'review_image' : review.image_url,
-                        'content'      : review.content,
-                        'created_at'   : review.created_at,
-                        'star_rating'  : review.star_rating
-                        } for review in reviews]
-
-        product_rate = product.review_set.aggregate(avg=Avg('star_rating'),count=Count('star_rating'))
-
-        PAGE_SIZE = 10
-        limit     = PAGE_SIZE * page
-        offset    = limit - PAGE_SIZE
-
-        if review_info[offset:limit] == []:
-            return JsonResponse({'result' : review_info[0:9], 'product_rate' : product_rate}, status=200)
-
-        return JsonResponse({'result' : review_info[offset:limit-1], 'product_rate' : product_rate}, status=200)
--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views      import SignUpView, AccountCheckView
 
 urlpatterns = [
-    path('/validaccount', AccountCheckView.as_view()),
+    path('/account-validator', AccountCheckView.as_view()),
     path('/signup', SignUpView.as_view())
 ]
