10ca37f908b9ed23e2674dc9acad798030aacfbd
AAAAA Ran <aaaaaran@AAAAAui-MacBookPro.local>
2021-06-16 15:23:14 +0900

    Add: 검색기능 (추가구현사항)

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from products.views import ProductList, ProductDetailView, ProductReviewVeiw
+from products.views import ProductList, ProductDetailView, ProductReviewVeiw, SearchView
 
 urlpatterns = [
     path('', ProductList.as_view()),
     path('/<int:product_id>', ProductDetailView.as_view()),
-    path('/<int:product_id>/review', ProductReviewVeiw.as_view())
+    path('/<int:product_id>/review', ProductReviewVeiw.as_view()),
+    path('/search', SearchView.as_view())  
 ]
