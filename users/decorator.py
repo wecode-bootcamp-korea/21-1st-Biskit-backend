ed19103f8efa31e1abb72f72c04884f242bd40e6
AAAAA Ran <aaaaaran@AAAAAui-MacBookPro.local>
2021-06-16 14:53:17 +0900

    데코레이터 수정

--- users/decorator.py
+++ users/decorator.py
@@ -1,6 +1,4 @@
 import jwt
-import json
-
 
 from django.http import JsonResponse
 
@@ -8,7 +6,7 @@ from biskit_settings import ALGORITHM, SECRET_KEY
 from .models         import User
 
 def login_decorator(func):
-    def wrapper(self, request):    
+    def wrapper(self, request, *args, **kwargs):    
         try:
             token        = request.headers.get('Authorization', None)
             token_data   = jwt.decode(token, SECRET_KEY, ALGORITHM)
@@ -16,10 +14,10 @@ def login_decorator(func):
             request.user = user
 
             if token is None : 
-                return JsonResponse({'message': 'INVALID_USER'}, status=400)
+                return JsonResponse({'message':'INVALID_USER'}, status=400)
         except jwt.DecodeError:
             return JsonResponse({'message':'DECODE_ERROR'}, status=400)
         except jwt.InvalidTokenError:
-            return JsonResponse({'message': 'INVALID_TOKEN'}, status = 400)
-        return func(self, request)
+            return JsonResponse({'message':'INVALID_TOKEN'}, status = 400)
+        return func(self, request, *args, **kwargs)
     return wrapper
--- users/views.py
+++ users/views.py
@@ -14,8 +14,11 @@ class AccountCheckView(View):
         data = json.loads(request.body)
 
         try:
-            account  = data['account']
+            account       = data['account']
+            account_Regex = '(^(?=.*[a-z])(?=.*\d)[a-z\d]{6,20}$)'
 
+            if not re.match (account_Regex, account):
+                  return JsonResponse({'message':'INVALID_FORMAT'}, status=400)             
             if User.objects.filter(account = account).exists():
                 return JsonResponse({'message':'ALREADY_EXISTS'}, status=400)
             return JsonResponse({'message':'SUCCESS'}, status=200) 
@@ -35,17 +38,21 @@ class SignUpView(View):
             email    = data['email']
             password = data['password']
             
-            account_Regex   = '(?!.*[\.\-\_]{2,})^[a-zA-Z0-9\.\-\_]{6,20}$'
-            password_Regex  = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,12}$'
-            email_Regex     = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-            mobile_Regex    = '\d{3,4}-\d{4}'
-            name_Regex      = '[가-힣]'
+            account_Regex   = '(^(?=.*[a-z])(?=.*\d)[a-z\d]{6,20}$)'
+            password_Regex  = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^~@$!%*#?&])[A-Za-z\d^~@$!#%*?&]{8,20}$'
+            mobile_Regex    = '^[0-9]{3}[0-9]{4}[0-9]{4}$'
+            email_Regex     = '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)'
+            name_Regex      = '^[가-힣]+'
             hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
             if ('' == account) or ('' == password):
                 return JsonResponse({'message':'VALUE_IS_EMPTY'}, status=400)
-            if not re.match (account_Regex, account) or (name_Regex, name) or (email_Regex, email) or (password_Regex, password) or (mobile_Regex, mobile):
-                return JsonResponse({'message':'INVALID_FORMAT'}, status=400)   
+            if not re.match(account_Regex, account) or\
+               not re.match(password_Regex, password) or\
+               not re.match(mobile_Regex, mobile) or\
+               not re.match(name_Regex, name) or\
+               not re.match(email_Regex, email):
+                return JsonResponse({'message':'INVALID_FORMAT'}, status=400)
             if User.objects.filter(account = account).exists():
                 return JsonResponse({'message':'ALREADY_EXISTS'}, status=400)
 
@@ -57,7 +64,7 @@ class SignUpView(View):
                 address  = address,
                 email    = email 
             ) 
-            return JsonResponse({'message':'SUCCESS'}, status= 201)
+            return JsonResponse({'message':'SUCCESS'}, status=201)
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
@@ -69,15 +76,14 @@ class SignInView(View):
             account  = data['account']
             password = data['password']
             user     = User.objects.get(account=account)
-          
+
             if not User.objects.filter(account=data['account']).exists():
-                return JsonResponse({'message':'INVALID_USER'}, status=401)
+                return JsonResponse({'message':'INVALID_USER'}, status=400)
             if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
                 return JsonResponse({'message':'WRONG_PASSWORD'}, status= 400)
             token = jwt.encode({'user_id':user.id}, SECRET_KEY, ALGORITHM)
-            return JsonResponse({'token':token}, status=200)
+            return JsonResponse({'token':token, 'message':'SUCCESS'}, status=200)
         except KeyError: 
             return JsonResponse ({'message':'KEY_ERROR'}, status=400) 
         except ValueError:
-            return JsonResponse({'message':'VALUE_ERROR'}, status=400)   
-        
+            return JsonResponse({'message':'VALUE_ERROR'}, status=400)
