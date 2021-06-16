8f968f4ac2a1933709007b783283f5d9ee2941d8
김민규 <skystar_pisces@gimmingyuui-MacBookAir.local>
2021-06-16 16:35:19 +0900

    Fix: 병합 충돌

--- orders/urls.py
+++ orders/urls.py
@@ -1,15 +1,8 @@
 from django.urls import path
-<<<<<<< HEAD
 
 from .views     import CartDeleteView, CartView
 
 urlpatterns = [
     path('', CartView.as_view()),
     path('/<int:product_id>', CartDeleteView.as_view())
-=======
-from .views      import OrderitemView
-
-urlpatterns = [
-    path('/cart',OrderitemView.as_view()),
->>>>>>> main
 ]
